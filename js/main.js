4ac79d09991fbcb8574c193d4e909ec538263ab9
Mohamed Samy <mohamedelbarody7@gmail.com>
2018-03-10 00:26:13 +0200

    small edit in tab number 6 and sucess run code

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 $(document).ready(function () {
     
     'use strict';
+
+  // Start Tabs
+
+    // Tab-1
     $(".tabs ul .tap-1").click(function(){
       $(".tabs ul .tap-2").removeClass("active");
       $(".tabs ul .tap-3").removeClass("active");
@@ -20,8 +24,8 @@ $(document).ready(function () {
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-1").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-2
     $(".tabs ul .tap-2").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-3").removeClass("active");
@@ -39,8 +43,8 @@ $(document).ready(function () {
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-2").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-3
     $(".tabs ul .tap-3").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-2").removeClass("active");
@@ -58,8 +62,8 @@ $(document).ready(function () {
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-3").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-4
     $(".tabs ul .tap-4").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-2").removeClass("active");
@@ -77,8 +81,8 @@ $(document).ready(function () {
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-4").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-5
     $(".tabs ul .tap-5").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-2").removeClass("active");
@@ -96,8 +100,8 @@ $(document).ready(function () {
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-5").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-6
     $(".tabs ul .tap-6").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-2").removeClass("active");
@@ -111,12 +115,12 @@ $(document).ready(function () {
       $("#tab-3").removeClass("active").addClass("disactive");
       $("#tab-4").removeClass("active").addClass("disactive");
       $("#tab-5").removeClass("active").addClass("disactive");
-      $("#tab-6").removeClass("active").addClass("disactive");
+      $("#tab-7").removeClass("active").addClass("disactive");
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-6").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-7
     $(".tabs ul .tap-7").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-2").removeClass("active");
@@ -134,8 +138,8 @@ $(document).ready(function () {
       $("#tab-8").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-7").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+    // Tab-8
     $(".tabs ul .tap-8").click(function(){
       $(".tabs ul .tap-1").removeClass("active");
       $(".tabs ul .tap-2").removeClass("active");
@@ -153,7 +157,8 @@ $(document).ready(function () {
       $("#tab-7").removeClass("active").addClass("disactive");
       $(this).addClass("active");
       $("#tab-8").removeClass("disactive").addClass("active");
-     // $(".tabs .hr .discuss").removeClass("disactive").addClass("active")
     });
+
+  // End Tabs
 
 });
